4d3aaa2bff7e1009563877b3967f16b5fbf2c018
Dani Wilson <danirae.powell@gmail.com>
2023-06-13 10:28:04 -0600

    add tests for most popular ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -9,6 +9,16 @@ RSpec.describe Carnival do
     @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
     @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
 
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @visitor3.add_preference(:thrilling)
+    @visitor2.add_preference(:gentle)
+
     @carnival = Carnival.new(14, [@ride1, @ride2, @ride3])
   end
 
@@ -16,5 +26,25 @@ RSpec.describe Carnival do
     expect(@carnival).to be_a(Carnival)
     expect(@carnival.duration).to eq(14)
     expect(@carnival.rides).to eq([@ride1, @ride2, @ride3])
+  end
+
+  it  'can list its most popular ride' do
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor3)
+
+    @ride2.board_rider(@visitor3)
+    @ride2.board_rider(@visitor3)
+
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+
+    expect(@carnival.most_popular_ride).to eq(@ride3)
   end
 end
