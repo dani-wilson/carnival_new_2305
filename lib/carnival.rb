2656c7a93e7d4206f5bd1fa3f85e9d440b6824ad
Dani Wilson <danirae.powell@gmail.com>
2023-06-13 11:33:40 -0600

    add most profitable rides and total revenue methods

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -8,6 +8,28 @@ class Carnival
   end
 
   def most_popular_ride
-    
+    popular_ride = {}
+    @rides.each do |ride|
+      ride_count = 0
+      ride.rider_log.each do |log|
+        ride_count += log[1]
+      end
+      popular_ride[ride] = ride_count
+    end
+    popular_ride.max_by {|ride, ride_count| ride_count}
+  end
+
+  def most_profitable_ride
+    @rides.max_by do |ride|
+      ride.revenue
+    end
+  end
+
+  def total_revenue
+    total_rev = 0
+    @rides.each do |ride|
+      total_rev += ride.revenue
+    end
+    return total_rev
   end
 end
