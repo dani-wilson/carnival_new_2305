61913de9ab521013691af7966be6b4aeb5f39515
Dani Wilson <danirae.powell@gmail.com>
2023-06-13 09:51:54 -0600

    fix test for spending money

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Visitor do
     expect(@visitor1).to be_a(Visitor)
     expect(@visitor1.name).to eq("Bruce")
     expect(@visitor1.height).to eq(54)
-    expect(@visitor1.spending_money).to eq("$10")
+    expect(@visitor1.spending_money).to eq(10)
   end
 
   it 'starts out with no preferences' do
