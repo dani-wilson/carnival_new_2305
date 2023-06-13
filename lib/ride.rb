9ca7c3945b124eb713f988fa57e303f002f28cd0
Dani Wilson <danirae.powell@gmail.com>
2023-06-13 09:15:01 -0600

    add revenue counter

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,12 +2,14 @@ class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
-              :excitement
+              :excitement,
+              :revenue
 
   def initialize(ride_hash)
     @name = ride_hash[:name]
     @min_height = ride_hash[:min_height]
     @admission_fee = ride_hash[:admission_fee]
     @excitement = ride_hash[:excitement]
+    @revenue = 0
   end
 end
