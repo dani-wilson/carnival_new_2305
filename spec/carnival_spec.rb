6306cc19386173fe4a696e3ec4155415eb5f3921
Dani Wilson <danirae.powell@gmail.com>
2023-06-13 11:33:51 -0600

    add tests for total revenue and most profitable ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,7 +1,7 @@
 require 'pry'
-require 'visitor'
-require 'ride'
-require 'carnival'
+require './lib/visitor'
+require './lib/ride'
+require './lib/carnival'
 
 RSpec.describe Carnival do
   before(:each) do
@@ -9,17 +9,18 @@ RSpec.describe Carnival do
     @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
     @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
 
+    @carnival = Carnival.new(14, [@ride1, @ride2, @ride3])
+    
     @visitor1 = Visitor.new('Bruce', 54, '$10')
     @visitor1.add_preference(:gentle)
-    @visitor2.add_preference(:thrilling)
+    @visitor1.add_preference(:thrilling)
     @visitor2 = Visitor.new('Tucker', 36, '$5')
     @visitor2.add_preference(:gentle)
     @visitor2.add_preference(:thrilling)
     @visitor3 = Visitor.new('Penny', 64, '$15')
     @visitor3.add_preference(:thrilling)
-    @visitor2.add_preference(:gentle)
-
-    @carnival = Carnival.new(14, [@ride1, @ride2, @ride3])
+    @visitor3.add_preference(:gentle)
+    
   end
 
   it 'exists and has attributes' do
@@ -46,5 +47,39 @@ RSpec.describe Carnival do
     @ride3.board_rider(@visitor3)
 
     expect(@carnival.most_popular_ride).to eq(@ride3) 
+    #it's returning the correct ride, there's just a minor difference??!?!!!
+  end
+
+  it 'can list its most profitable ride' do
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor3)
+
+    @ride2.board_rider(@visitor3)
+    @ride2.board_rider(@visitor3)
+
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+
+    expect(@carnival.most_profitable_ride).to eq(@ride3)
+  end
+
+  it 'can tell the total revenue of the carnival' do
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor3)
+    @ride2.board_rider(@visitor3)
+    @ride2.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    expect(@carnival.total_revenue).to eq(20)
   end
 end
