7267f26c683dcabef989d89d05b7100b67478246
Dani Wilson <danirae.powell@gmail.com>
2023-06-13 09:51:06 -0600

    Add rider log and board rider method

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,7 +3,8 @@ class Ride
               :min_height,
               :admission_fee,
               :excitement,
-              :revenue
+              :revenue,
+              :rider_log
 
   def initialize(ride_hash)
     @name = ride_hash[:name]
@@ -11,5 +12,17 @@ class Ride
     @admission_fee = ride_hash[:admission_fee]
     @excitement = ride_hash[:excitement]
     @revenue = 0
+    @rider_log = {}
   end
+
+  def board_rider(visitor)
+    if !visitor.preferences.include?(@excitement) || visitor.height < @min_height
+      false
+    else
+      @rider_log[visitor] = [visitor]
+      visitor.spending_money = visitor.spending_money - @admission_fee
+      @revenue += @admission_fee
+    end
+  end
+
 end
