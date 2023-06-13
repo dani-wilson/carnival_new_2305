93083a1236e566e6c137e57f2cb9c0fa6179a5fc
Dani Wilson <danirae.powell@gmail.com>
2023-06-13 09:51:39 -0600

    change spending money string to integer

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,13 +1,14 @@
 class Visitor
-  attr_reader :name,
-              :height,
-              :spending_money,
-              :preferences
+  attr_reader   :name,
+                :height,
+                :spending_money,
+                :preferences
+  attr_accessor :spending_money
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
-    @spending_money = spending_money
+    @spending_money = spending_money.gsub(/\D/,'').to_i
     @preferences = []
   end
 
